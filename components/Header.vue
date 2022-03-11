30d74efa4f06fb886baa4a0224122db87f2eca00
Ricardo Serrano Pena <ricardo30499@gmail.com>
2022-03-11 08:43:42 +0700

    added content

---
+++ components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <nav>
+      <img src="~/assets/images/Title-logo.png" />
+      <ul>
+        <li>
+          <h2><a href="#">About</a></h2>
+        </li>
+        <li>
+          <h2><a href="#">Philosopy</a></h2>
+        </li>
+        <li>
+          <h2><a href="#">Newsletter</a></h2>
+        </li>
+        <li>
+          <h2><a href="#">Apply</a></h2>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+header {
+  height: 10vh;
+  background-color: blue;
+  color: red;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,205 @@
 <template>
-  <Tutorial />
+  <main>
+    <section class="hero">
+      <header>
+        <h2 class="hero-title">
+          gieo nhân tốt,<br />
+          gặt quả lành
+        </h2>
+      </header>
+    </section>
+
+    <section class="cards">
+      <div class="card">
+        <h3 class="card-title">Licensed Child Care</h3>
+        <p class="card-body">
+          Cupcake ipsum dolor sit amet jelly beans gummies. Sweet chupa chups
+          chocolate oat cake caramels jujubes muffin marshmallow. Gingerbread
+          oat cake caramels icing topping.
+        </p>
+      </div>
+      <div class="card">
+        <h3 class="card-title">High Quality Learning</h3>
+        <p class="card-body">
+          Cupcake ipsum dolor sit amet cotton candy candy tootsie roll.
+          Marshmallow candy canes sugar plum bonbon sugar plum. Sweet bonbon
+          soufflé dessert apple pie. Jelly-o
+        </p>
+      </div>
+      <div class="card">
+        <h3 class="card-title">The Talented Staff</h3>
+        <p class="card-body">
+          Cupcake ipsum dolor sit amet lollipop marshmallow. Sweet chocolate
+          cake liquorice dessert jelly-o cake jujubes shortbread. Apple pie
+          sugar plum gingerbread
+        </p>
+      </div>
+    </section>
+    <section class="about-us">
+      <header><h2 class="section-title">About Us</h2></header>
+      <div class="section-body">
+        <div class="body-left">
+          <p>
+            Cupcake ipsum dolor sit amet. Powder donut gummies jelly beans
+            gingerbread. Halvah sugar plum caramels candy canes pastry cake.
+            Gingerbread cookie bear claw jelly ice cream jelly-o tart. Tart
+            sesame snaps caramels lemon drops chocolate icing cake toffee
+            chocolate bar. Pudding jelly-o carrot cake gummies chupa chups.
+            Gingerbread brownie halvah donut cookie chocolate cake jelly beans
+            macaroon.
+          </p>
+          <p>
+            Chocolate bar icing sugar plum cupcake gingerbread candy canes.
+            Wafer danish icing chocolate bar topping marshmallow muffin brownie.
+            Bonbon chocolate bar oat cake bonbon gummi bears. Jelly halvah
+            bonbon chocolate cake donut chocolate cake chupa chups. Tiramisu
+            caramels candy chupa chups chocolate. Marshmallow tootsie roll gummi
+            bears biscuit caramels lollipop sweet roll gummi bears. Candy canes
+            sweet roll halvah dessert dragée jujubes gummi bears lemon drops.
+            Dragée icing dessert chupa chups halvah brownie fruitcake chupa
+            chups. Sesame snaps wafer carrot cake powder shortbread wafer bear
+            claw.
+          </p>
+          <a href="" class="btn btn-primary">Learn More</a>
+        </div>
+        <div class="body-right">
+          <img src="~/assets/images/kids-eating.jpg" />
+        </div>
+      </div>
+    </section>
+    <section class="our-philosophy">
+      <header>
+        <h2 class="section-title">Our Philosophy</h2>
+      </header>
+      <div class="section-body">
+        <p>
+          Cupcake ipsum dolor sit amet apple pie marzipan cookie. Pie candy
+          canes ice cream dragée brownie. Carrot cake bear claw pie liquorice
+          toffee gummies.
+        </p>
+        <p>
+          Topping candy canes pudding biscuit lollipop pudding apple pie
+          chocolate cake. Macaroon gummi bears cake sugar plum croissant apple
+          pie dragée. Candy jelly beans biscuit candy canes dessert ice cream
+          pie. Icing tart macaroon gingerbread toffee bear claw wafer.
+        </p>
+        <p>
+          Gingerbread shortbread gummies carrot cake tootsie roll oat cake
+          wafer. Croissant sweet roll cheesecake pie ice cream. Wafer cupcake
+          croissant cotton candy carrot cake candy cupcake. Powder jelly-o
+          danish wafer tiramisu.
+        </p>
+        <a href="" class="btn btn-primary">Learn More</a>
+      </div>
+    </section>
+    <section class="testimonials">
+      <header>
+        <h2 class="section-title">Testimonials</h2>
+      </header>
+      <div class="section-body">
+        <div class="testimonial-cards">
+          <div class="testimonial-card">
+            <img src="~/assets/images/person1.jpg" />
+            <h4 class="testimonial-name">Name Lastname</h4>
+            <p class="testimonial-role">Parent</p>
+            <p class="testimonial-quote">
+              Cupcake ipsum dolor sit amet muffin. Tiramisu dessert wafer
+              gummies chocolate cake pastry chocolate bar soufflé. Jujubes cake
+              tiramisu sweet roll chocolate.
+            </p>
+          </div>
+          <div class="testimonial-card">
+            <img src="~/assets/images/person2.jpg" />
+            <h4 class="testimonial-name">Name Lastname</h4>
+            <p class="testimonial-role">Director</p>
+            <p class="testimonial-quote">
+              Cupcake ipsum dolor sit amet muffin. Tiramisu dessert wafer
+              gummies chocolate cake pastry chocolate bar soufflé. Jujubes cake
+              tiramisu sweet roll chocolate.
+            </p>
+          </div>
+          img
+          <div class="testimonial-card">
+            <img src="~/assets/images/person3.jpg" />
+            <h4 class="testimonial-name">Name Lastname</h4>
+            <p class="testimonial-role">Major</p>
+            <p class="testimonial-quote">
+              Cupcake ipsum dolor sit amet muffin. Tiramisu dessert wafer
+              gummies chocolate cake pastry chocolate bar soufflé. Jujubes cake
+              tiramisu sweet roll chocolate.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="newsletter-resources">
+      <div class="newsletter">
+        <heading>
+          <h2>Newsletter</h2>
+        </heading>
+        <div class="newsletter-card">
+          <img src="~/assets/images/news1.jpg" />
+          <div class="card-date">30<br />T2</div>
+          <h3 class="newsletter-card-title">What's on the board</h3>
+          <p class="newsletter-card-body">
+            Cupcake ipsum dolor sit amet carrot cake tiramisu caramels tootsie
+            roll. Chocolate lollipop bonbon pastry jelly wafer lemon drops.
+            Sweet cheesecake cake shortbread gummies jujubes tootsie roll. Bear
+            claw danish sweet roll candy chocolate bar.
+          </p>
+          <a href="" class="">Keep Reading</a>
+        </div>
+        <a href="" class="btn btn-primary">Read More News</a>
+      </div>
+      <div class="resources">
+        <heading>
+          <h2>Resources</h2>
+        </heading>
+        <div class="resources-cards">
+          <div class="resources-card">
+            <img src="~/assets/images/document-icon.png" alt="" />
+            <p class="resources-card-title">Resources on Parenting</p>
+          </div>
+          <div class="resources-card">
+            <img src="~/assets/images/document-icon.png" alt="" />
+            <p class="resources-card-title">Resources on Parenting</p>
+          </div>
+          <div class="resources-card">
+            <img src="~/assets/images/document-icon.png" alt="" />
+            <p class="resources-card-title">Resources on Parenting</p>
+          </div>
+          <div class="resources-card">
+            <img src="~/assets/images/document-icon.png" alt="" />
+            <p class="resources-card-title">Resources on Parenting</p>
+          </div>
+        </div>
+        <a href="" class="btn btn-primary">Get More Resources</a>
+      </div>
+    </section>
+    <section class="enroll">
+      <div class="enroll-card">
+        <img src="~/assets/images/apply1.jpg" />
+        <div class="apply-form">
+          <Heading>
+            <h2 class="form-title">Enroll Today</h2>
+          </Heading>
+          <form>
+            <div class="form-group">
+              <label for="first-name">First Name</label>
+              <input type="text" id="first-name" placeholder="First Name" />
+              <label for="last-name">Last Name</label>
+              <input type="text" id="last-name" placeholder="Last Name" />
+            </div>
+            <label for="email">Email</label>
+            <input type="email" id="email" />
+            <label for="phone">Phone</label>
+            <input type="text" id="phone" />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
