3395687cc6aa72ce57076aa544bb5c82daab4414
Ricardo Serrano Pena <ricardo30499@gmail.com>
2022-03-11 15:43:16 +0700

    added Footer

---
+++ components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer>
+    <img src="~/assets/images/ddhk-logo.png" />
+    <div class="footer-contact-us">
+      <h4>Contact Us</h4>
+      <img src="~/assets/images/phone-alt-solid.svg" />
+      <p>(323)555-3245</p>
+      <img src="~/assets/images/envelope-solid.svg" />
+      <p>daodokindergarten@gmail.com</p>
+      <img src="~/assets/images/map-marker-solid.svg" />
+      <p>Street Name 123 - New York Elevent</p>
+    </div>
+    <div class="footer-working-hours">
+      <h4>Working Hours</h4>
+      <p>Monday to Friday</p>
+      <p>Open from 9am to 6pm</p>
+      <p>Holidays - Closed</p>
+      <p>Weekends - Closed</p>
+    </div>
+    <div class="footer-subscribe-newsletter">
+      <h4>Subscribe to Our Newsletter</h4>
+      <form>
+        <label for="subscribe-email">Email</label>
+        <input tpye="text" id="subscribe-email" />
+        <input type="submit" value="Subscribe" />
+      </form>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
