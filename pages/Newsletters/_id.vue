ccdc49a8f95184372ee9a7960ecac5b31ce95f18
Ricardo Serrano Pena <ricardo30499@gmail.com>
2022-03-31 09:36:23 +0700

    added styles + refactor for newsletter

--- pages/Newsletters/_id.vue
+++ pages/Newsletters/_id.vue
@@ -1,6 +1,17 @@
 <template>
-  <main v-if="page">
-    <div v-html="page"></div>
+  <main>
+    <article v-if="content" class="newsletter-page">
+      <heading class="newsletter-heading">
+        <h1 class="title">{{ title }}</h1>
+        <h3 class="subtitle">
+          {{ metadata.description }}
+        </h3>
+        <div class="newsletter-hero-container">
+          <img :src="metadata.hero.url" alt="" class="newsletter-hero" />
+        </div>
+      </heading>
+      <div v-html="content" class="newsletter-content"></div>
+    </article>
   </main>
 </template>
 
@@ -8,27 +19,40 @@
 import getObject from '~/queries/getNewsletter'
 
 export default {
-  async asyncData({ app, route, redirect }) {
-    let data = {}
+  async asyncData({ app, params, redirect }) {
+    const client = app.apolloProvider.defaultClient
+    const { id } = params
     try {
-      const d = await app.apolloProvider.defaultClient.query({
+      const res = await client.query({
         query: getObject,
         variables: {
           bucket_slug: 'dau-do-house-production',
           read_key: 'fk6S5xVNuPsrf3WchtJhjgy2vr6OIxkkpWoWcg1KPbW4xnUh8s',
-          object_id: route.params.id,
+          object_id: id,
         },
       })
-      const data = d.data
-      console.log('data', data)
+
+      const newsletter = res.data.getObject
+      console.log('data', newsletter)
+      const { title, content, metadata } = newsletter
+
       return {
-        page: data.getObject.content,
+        title,
+        content,
+        metadata,
       }
     } catch (error) {
       console.log('error', error)
-      // redirect('/')
+      redirect('/Newsletters')
     }
   },
+  // data() {
+  //   return {
+  //     title: '',
+  //     date: '',
+  //     hero_url: '',
+  //   }
+  // },
 }
 </script>
 
